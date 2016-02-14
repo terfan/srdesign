2e81402ef8bd5eae601e62d3d26d806fd23084c2
Teresa Fan <teresaefan@gmail.com>
2016-02-13 20:07:53 -0500

    adding dummy draw page

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var root = this;
 var ua = root.navigator.userAgent;
 var landingPage = document.getElementById( 'landing_page' );
-var launchButton = document.getElementById( 'launch_button' );
+var launchCanvasButton = document.getElementById( 'canvas_button' );
+var launchDrawButton = document.getElementById( 'draw_button' );
 var popupWindow = document.getElementById( 'popup_window' );
 var popupCloseButton = document.getElementById( 'popup_close_button' );
 var tryAnywayLink = document.getElementById( 'try_anyway' );
 
-  	var has = {
+var has = {
 
     // Mobile Detection
     Android: !!ua.match(/Android/ig),
@@ -28,7 +29,7 @@ var tryAnywayLink = document.getElementById( 'try_anyway' );
 
     webgl: (function() { try { return !!window.WebGLRenderingContext && !!(document.createElement('canvas').getContext('webgl') || document.createElement('canvas').getContext('experimental-webgl')); } catch(e) { return false; } })(),
 
-  	};
+};
 
   	has.mobile = has.Android || has.Blackberry || has.iOS || has.OperaMini || has.Windows || has.WebOS;
 
@@ -47,18 +48,24 @@ document.addEventListener('DOMContentLoaded', function() {
 	var canRun = true;
 	if (canRun) {
       if (!has.mobile) {
-      	launchButton.addEventListener( 'click', onOpenPopupClicked, true );
+      	launchCanvasButton.addEventListener( 'click', onOpenPopupClicked, true );
       	popupCloseButton.addEventListener( 'click', onClosePopupClicked, true );
-        tryAnywayLink.addEventListener( 'click', onLaunchClicked, true );
+        tryAnywayLink.addEventListener( 'click', onLaunchCanvasClicked, true );
       } else {
-      	launchButton.addEventListener( 'click', onLaunchClicked, true );
+      	launchCanvasButton.addEventListener( 'click', onLaunchCanvasClicked, true );
       }
+      launchDrawButton.addEventListener( 'click', onLaunchDrawClicked, true);
 	}
 });
 
-function onLaunchClicked(ev) {
-	console.log("hi");
+function onLaunchCanvasClicked(ev) {
+	console.log("launching canvas");
   window.location = '/viewer.html';
+}
+
+function onLaunchDrawClicked(ev) {
+	console.log("launching drawing tool");
+  window.location = '/draw.html';
 }
 
 function onOpenPopupClicked(ev) {
